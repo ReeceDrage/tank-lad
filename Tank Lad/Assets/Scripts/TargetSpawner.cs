2c3b80e8ed6c89f4186b63f1f90df0a7ac0c3bfa
PunchiSquid <reece.drage@gmail.com>
2019-08-28 18:48:29 +0100

    Created TargetSpawner class. Starts a coroutine that triggers every 5 seconds to create new targets.

---
+++ Tank Lad/Assets/Scripts/TargetSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSpawner : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> spawnPoints;
+    [SerializeField] private GameObject target;
+
+    private bool spawning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoints = new List<GameObject>();
+
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            spawnPoints.Add(this.transform.GetChild(i).gameObject);
+        }
+
+        StartCoroutine(CheckSpawnPoints());
+    }
+
+    IEnumerator CheckSpawnPoints()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f);
+            foreach(GameObject s in spawnPoints)
+            {
+                if (s.transform.childCount == 0)
+                {
+                    Instantiate(target, s.transform);
+                }
+            }
+        }
+    }
+}
