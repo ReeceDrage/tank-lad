3d512178b1c21d0b34878ff347e7a510e5531547
PunchiSquid <reece.drage@gmail.com>
2019-08-28 20:23:30 +0100

    Added variation to the shot time in the TurretController script

--- Tank Lad/Assets/Scripts/TurretController.cs
+++ Tank Lad/Assets/Scripts/TurretController.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class TurretController : MonoBehaviour
 {
     [SerializeField] float turretRotationSpeed = 10f;
+    [SerializeField] float timeBetweenShots = 3f;
+    [SerializeField] float shotTimeVariation = 0.2f;
+    [SerializeField] private GameObject shot;
+
     private GameObject player;
+    private GameObject firingPoint;
+    private bool canShoot = true;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player Tank");
+        firingPoint = this.transform.Find("Fire Point").gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         RotateToFacePlayer();
+        Shoot();
     }
 
     private void RotateToFacePlayer()
@@ -28,5 +36,30 @@ public class TurretController : MonoBehaviour
         float angle = Mathf.Atan2(relativePosition.normalized.y, relativePosition.normalized.x) * Mathf.Rad2Deg - 90f;
         Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, q, Time.deltaTime * turretRotationSpeed);
+    }
+
+    private void Shoot()
+    {
+        if (canShoot == true)
+        {
+            // Create shot object with the current rotation of the turret. Set the mask property to be this layer mask so it doesn't collide with this object
+            GameObject shotFired = Instantiate(shot, firingPoint.transform.position, this.transform.rotation);
+            shotFired.GetComponent<CannonShot>().mask = this.gameObject.layer;
+
+            // Prevents repeated shots
+            canShoot = false;
+            StartCoroutine(ResetShot());
+        }
+    }
+
+    /// <summary>
+    /// Resets the canShoot boolean to the initial true value. Allows for shot spacing.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator ResetShot()
+    {
+        float variedTime = timeBetweenShots + Random.Range(-shotTimeVariation, shotTimeVariation);
+        yield return new WaitForSeconds(variedTime);
+        canShoot = true;
     }
 }
