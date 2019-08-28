be7aa15524b076d883c7a5fc7ff7d2525900659c
PunchiSquid <reece.drage@gmail.com>
2019-08-28 16:14:09 +0100

    Added basic script for cannon shots.

---
+++ Tank Lad/Assets/Scripts/CannonShot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonShot : MonoBehaviour
+{
+    [SerializeField] private float shotVelocity = 15f;
+    [SerializeField] public LayerMask mask;
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = this.transform.up * shotVelocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer != mask)
+        {
+            print("Shot hit object. Destroying shot.");
+            Destroy(this.gameObject);
+        }
+    }
+}
