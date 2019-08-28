5dfdf9c2580dc028c1754031d73e545a86b90655
PunchiSquid <reece.drage@gmail.com>
2019-08-28 17:47:23 +0100

    CannonShot now instantiates a smoke cloud prefab on contact with a collider

--- Tank Lad/Assets/Scripts/CannonShot.cs
+++ Tank Lad/Assets/Scripts/CannonShot.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class CannonShot : MonoBehaviour
 {
     [SerializeField] private float shotVelocity = 15f;
+    [SerializeField] private GameObject smoke;
     [SerializeField] public LayerMask mask;
+
     private Rigidbody2D rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+
         rigidbody.velocity = this.transform.up * shotVelocity;
     }
 
@@ -20,6 +23,7 @@ public class CannonShot : MonoBehaviour
         if (collision.gameObject.layer != mask)
         {
             print("Shot hit object. Destroying shot.");
+            Instantiate(smoke, this.transform.position, this.transform.rotation);
             Destroy(this.gameObject);
         }
     }
